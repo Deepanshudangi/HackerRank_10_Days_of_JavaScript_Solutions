a1c114cdbce1094fff4325cba17532be4da70ac1
deepanshudangi <deepanshudangi10@gmail.com>
2021-06-10 20:01:15 +0530

    day 5 solutions

---
+++ Day 5/Day 5_Inheritance.js
@@ -0,0 +1,16 @@
+
+
+
+Rectangle.prototype.area = function () {
+    return this.w * this.h;
+};
+
+class Square extends Rectangle {
+    constructor(s) {
+        super(s);
+        this.h = s;
+        this.w = s;
+    }
+}
+
+
